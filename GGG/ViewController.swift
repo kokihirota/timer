e76363ccc2b9071f60de6af323ce2fa8dba88018
s15ti050 <s15ti050@s15ti050noMacBook-Pro.local>
2017-09-08 22:03:56 +0900

    Initial Commit

---
+++ GGG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GGG
+//
+//  Created by s15ti050 on 2017/09/08.
+//  Copyright © 2017年 s15ti050. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
